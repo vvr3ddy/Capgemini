1a1bfec9e4b7cbe44d062e61a6786225625c9fb3
vvr3ddy <vvr-rockstar@bmsit.in>
2021-03-16 19:34:29 +0530

    :octocat: Add gitignore
    Because we need to keep our actual stuff private
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/Main01/Source.java
+++ src/Main01/Source.java
@@ -2,33 +2,34 @@ package Main01;
 
 import java.util.*;
 
-public class Source {
-	public static int[] solution(int[] data, int n) {
-		LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
-		for (int i : data) {
-			if (!map.containsKey(i)) {
-				map.put(i, 1);
-			} else {
-				map.put(i, map.get(i) + 1);
-			}
+class ArrayListOps {
+	public ArrayList<Integer> makeArrayListInt(int n) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			list.add(0);
 		}
-		List<Integer> arr = new ArrayList<>();
-		for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-			if (e.getValue() > n) {
-
-			} else {
-				arr.add(e.getKey());
-			}
-		}
-		int[] res = new int[arr.size()];
-		for(int i=0;i<arr.size();i++) {
-			res[i] = arr.get(i);
-			
-		}
-		return res;
+		return list;
 	}
 
-	public static void main(String[] args) {
+	public ArrayList<Integer> reverseList(ArrayList<Integer> list) {
+		Collections.reverse(list);
+		return list;
+	}
 
+	public ArrayList<Integer> changeList(ArrayList<Integer> list, int m, int n) {
+		int elem = 0;
+		for (int i = 0; i < list.size(); i++) {
+			elem = list.get(i);
+			if (elem == m) {
+				list.set(i, n);
+			}
+		}
+		return list;
+	}
+
+}
+
+public class Source {
+	public static void main(String[] args) {
 	}
 }
